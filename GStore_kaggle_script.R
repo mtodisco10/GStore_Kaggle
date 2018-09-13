cc42adfcaf5806682620d3e7463e097a53e12725
Mike Todisco <mtodisco@splashthat.com>
2018-09-13 17:19:35 -0400

    data is reading in

--- GStore_kaggle_script.R
+++ GStore_kaggle_script.R
@@ -1,2 +1,13 @@
-print('I\'m going to win!! and take all the $$')
+#Disable Scientific Notation
+options(scipen=999)
+library(jsonlite)
 
+#Read in the Data
+test_data <- read.csv('dataFiles/test.csv', stringsAsFactors = FALSE)
+train_data <- read.csv('dataFiles/train.csv', stringsAsFactors = FALSE)
+sample_submission <- read.csv('dataFiles/sample_submission.csv', stringsAsFactors = FALSE)
+  
+
+for (visit in train_data$totals[1000:1001]){
+  print(fromJSON(visit)$transactionRevenue)
+}
