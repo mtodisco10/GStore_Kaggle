d9a93445cd6b07d8327cfc4999e4824617fa3990
Mike Todisco <mtodisco@splashthat.com>
2018-09-18 18:23:33 -0400

    parsing the totals json field

--- GStore_kaggle_script.R
+++ GStore_kaggle_script.R
@@ -8,6 +8,18 @@ train_data <- read.csv('dataFiles/train.csv', stringsAsFactors = FALSE)
 sample_submission <- read.csv('dataFiles/sample_submission.csv', stringsAsFactors = FALSE)
   
 
-for (visit in train_data$totals[1000:1001]){
-  print(fromJSON(visit)$transactionRevenue)
+full_visit_df <- data.frame(visits=character(),
+                            hits=character(),
+                            pageviews=character(),
+                            newVisits=character(),
+                            transactionRevenue=character())
+
+
+for (visit in train_data$totals[1:1000]){
+  visit_df <- data.frame(fromJSON(visit))
+  visit_df$newVisits <- ifelse("newVisits" %in% names(visit_df), visit_df$newVisits, 0)
+  visit_df$transactionRevenue <- ifelse("transactionRevenue" %in% names(visit_df), visit_df$transactionRevenue, 0)
+  visit_df <- visit_df[c('visits','hits','pageviews','newVisits','transactionRevenue')]
+  full_visit_df <- rbind(full_visit_df, visit_df)
 }
+
