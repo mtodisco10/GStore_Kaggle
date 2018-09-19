9683f0b5c3bd7ede17f5113ec218f50caa311034
Mike Todisco <mtodisco@splashthat.com>
2018-09-19 10:34:52 -0400

    easier and quicker way to parse json fields

--- GStore_kaggle_script.R
+++ GStore_kaggle_script.R
@@ -7,19 +7,9 @@ test_data <- read.csv('dataFiles/test.csv', stringsAsFactors = FALSE)
 train_data <- read.csv('dataFiles/train.csv', stringsAsFactors = FALSE)
 sample_submission <- read.csv('dataFiles/sample_submission.csv', stringsAsFactors = FALSE)
   
-
-full_visit_df <- data.frame(visits=character(),
-                            hits=character(),
-                            pageviews=character(),
-                            newVisits=character(),
-                            transactionRevenue=character())
-
-
-for (visit in train_data$totals[1:1000]){
-  visit_df <- data.frame(fromJSON(visit))
-  visit_df$newVisits <- ifelse("newVisits" %in% names(visit_df), visit_df$newVisits, 0)
-  visit_df$transactionRevenue <- ifelse("transactionRevenue" %in% names(visit_df), visit_df$transactionRevenue, 0)
-  visit_df <- visit_df[c('visits','hits','pageviews','newVisits','transactionRevenue')]
-  full_visit_df <- rbind(full_visit_df, visit_df)
-}
+#Parsing and flattening the json fields
+device_df <- paste("[", paste(train_data$device, collapse = ","), "]") %>% fromJSON(flatten = T)
+geo_network_df <- paste("[", paste(train_data$geoNetwork, collapse = ","), "]") %>% fromJSON(flatten = T)
+totals_df <- paste("[", paste(train_data$totals, collapse = ","), "]") %>% fromJSON(flatten = T)
+traffic_source_df <- paste("[", paste(train_data$trafficSource, collapse = ","), "]") %>% fromJSON(flatten = T)
 
